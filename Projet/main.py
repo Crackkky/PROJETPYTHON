e3e74840c523471ad4ee102cb6d46f6d509f9dba
arnaudg <arnaud.garrigue3@etu.univ-lorraine.fr>
2020-04-24 22:00:16 +0200

    cool stuff

--- Projet/main.py
+++ Projet/main.py
@@ -1,62 +1,61 @@
 from random import *
 from Projet.plate import *
 
+
+# FUNCTION DEF
+
+
+def chooseIndex():
+    while True:
+        index = int(input('Choose a plate number : '))
+        if index > 6 or index <= 0:
+            print('Erreur index ! \n')
+        else:
+            print('You\'ve chosen : ' + str(selectedPlates[index - 1].getNumber()))
+            return index - 1
+
+
+def chooseOperator():
+    while True:
+        operator = input('Choose an operator in [+ \\ - \\ * \\ /] : ')
+        if operator != '+' and operator != '-' and operator != '*' and operator != '/':  # TODO Dur dur
+            print('Erreur operator ! \n')
+        else:
+            print('You\'ve chosen : ' + operator)
+            return operator
+
+
+# FUNCTION DEF
+
 print('Welcome in the training mode !')
 
 goal = randint(100, 999)
 possiblePlates = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 25, 50, 50, 75, 75, 100, 100]
-#TODO en dur
+# TODO durian
 selectedPlates = []
 
+# Generate plates
 for i in range(1, 7):
-    plateNumber = randint(0, 27) #TODO 27 en dur
-    selectedPlates.append(plate(possiblePlates[plateNumber]))
+    plateNumber = randint(0, 27)  # TODO 27 durrrrrrrrrr
+    selectedPlates.append(Plate(possiblePlates[plateNumber]))
 
-selectedPlates = [plate(2), plate(4), plate(25), plate(1), plate(75), plate(9)]
+selectedPlates = [Plate(2), Plate(4), Plate(25), Plate(1), Plate(75), Plate(8)]
 
+while True:
+    # Choose a plate
+    printPlateArray(selectedPlates)
+
+    userSelection1 = chooseIndex()
+    userSelectionOp = chooseOperator()
+    userSelection2 = chooseIndex()
+
+    # Add 2 plates
+    try:
+        operationArray(selectedPlates, userSelection1, userSelection2, userSelectionOp)
+        break
+    except Exception as e:
+        print(e)
+    print('')
+
+print('')
 printPlateArray(selectedPlates)
-
-selectedPlates[1].operation(selectedPlates, 2)
-printPlateArray(selectedPlates)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Projet/plate.py
+++ Projet/plate.py
@@ -1,67 +1,43 @@
-
-
-
 def printPlateArray(array):
     tabString = []
     for i in range(0, len(array)):
         tabString.append(array[i].getNumber())
     print(tabString)
 
-class plate():
+
+def operationArray(plateArray, index1, index2, operator):
+    # print('Vous essayez de faire : ' + str(plateArray[index1].getNumber()) + operator
+    # + str(plateArray[index2].getNumber()))
+    try:
+        plateArray.append(plateArray[index1].operation(plateArray[index2], operator))
+    except Exception as e:
+        raise e
+
+    if index1 < index2:
+        plateArray.remove(plateArray[index2])
+        plateArray.remove(plateArray[index1])
+    else:
+        plateArray.remove(plateArray[index1])
+        plateArray.remove(plateArray[index2])
+
+
+class Plate:
     def __init__(self, number):
         self.number = number
 
     def getNumber(self):
         return self.number
 
-    def operation(self, plateArray, index): #TODO add parameter to choose operation, evaluate?
-        plateArray.append(self.add(plateArray[index]))
-        plateArray.remove(plateArray[index])
-        plateArray.remove(self)
+    # Do : "first plate <operator> second plate."
+    def operation(self, plate2, operator):
+        if operator == '/' and (self.getNumber() % plate2.getNumber() != 0):
+            # print('Reste = ' + str(plate2.getNumber() % self.getNumber()))
+            raise Exception('Erreur le reste de le division n\'est pas nul!')
 
-    def add(self, plate2):
-        return plate(self.getNumber() + plate2.getNumber())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if operator == '-' and (self.getNumber() - plate2.getNumber() < 0):
+            print('<0 !!!!!!')
+            print(plate2.getNumber() - self.getNumber())
+            return Plate(plate2.getNumber() - self.getNumber())
+        else:
+            print(int(eval(str(self.getNumber()) + operator + str(plate2.getNumber()))))
+            return Plate(int(eval(str(self.getNumber()) + operator + str(plate2.getNumber()))))
