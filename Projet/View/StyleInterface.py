ae0a3e2cca9f2f374520fe4f0e6e816794f5e6b8
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2020-05-06 03:07:05 +0200

    Classe des themes par défaut de la vue

---
+++ Projet/View/StyleInterface.py
@@ -0,0 +1,24 @@
+#Cette classe permet d'appliquer facilement un theme global a des widgets
+
+def rootStyle(root) :
+    root.geometry('500x250')
+    root.configure(background='gray')
+
+def buttonStyle(button) :
+    types = ['bg', 'fg', 'activeforeground', 'activebackground', 'borderwidth']
+    values = ['black', 'pink', 'black', 'pink', 3]
+    apply(types, values, button)
+
+
+def buttonStyleOnArray(buttons) :
+    for i in buttons :
+        buttonStyle(i)
+
+def frameStyle(frame) :
+    types = ['borderwidth', 'bg']
+    values = ['10', 'pink']
+    apply(types, values, frame)
+
+def apply(lt, lv, object) :
+    for t, v in zip(lt, lv) :
+        object[t] = v
