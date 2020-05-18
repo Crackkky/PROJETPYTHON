d1c6d551b4a3d743c38607c8426b8ea24246bca1
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2020-05-19 01:00:13 +0200

    Version merge possiblement bug ?

--- Projet/Model/tileModel.py
+++ Projet/Model/tileModel.py
@@ -1,13 +1,19 @@
 import itertools
 from Projet.Model.util import *
 
+DEBUG_VAR = True
+
 
 class TileModel:
     def __init__(self):
         self.empty()
 
     def initGame(self):
-        self.goal, self.originalTiles = generateGoalTiles(100, 999, 27)
+        if DEBUG_VAR:
+            self.goal = 170
+            self.originalTiles = [Tile(10), Tile(10), Tile(25), Tile(25), Tile(50), Tile(50)]
+        else:
+            self.goal, self.originalTiles = generateGoalTiles(100, 999, 27)
         self.selectedTiles = self.originalTiles.copy()
         self.lenSelectedTile = len(self.selectedTiles)
 
@@ -17,8 +23,8 @@ class TileModel:
         self.selectedTiles = []
         self.lenSelectedTile = None
 
-    def getDifference(self, value = -1):
+    def getDifference(self, value=-1):
         if self.lenSelectedTile == 1:
             return abs(self.goal - self.selectedTiles[0].getNumber())
         if value is not -1:
-            return abs(self.goal-value)
+            return abs(self.goal - value)
